722ab44877894318279f8132233b792a2e552150
stanko75 <stanko@milosev.com>
2014-11-11 18:28:45 +0100

    Model

---
+++ responsiveWebDesign/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="responsiveWebDesign.MvcApplication" Language="C#" %>
---
+++ responsiveWebDesign/Models/RealEstate.cs
@@ -0,0 +1,22 @@
+﻿namespace responsiveWebDesign.Models
+{
+  using System;
+
+  public class RealEstate
+  {
+    public int Id { get; set; }
+    public string Company { get; set; }
+    public string City { get; set; }
+    public string Location { get; set; }
+    public string Type { get; set; }
+    public int SquareMeters { get; set; }
+    public float Price { get; set; }
+    public string Link { get; set; }
+    public int Page { get; set; }
+    public bool Active { get; set; }
+    public DateTime UpdateTime { get; set; }
+    public DateTime UpdateDate { get; set; }
+    public DateTime InsertTime { get; set; }
+    public DateTime InsertDate { get; set; }
+  }
+}
