0ce5285383538f3855cd79dc6f0433b51129252d
stanko75 <stanko@milosev.com>
2014-11-17 00:05:04 +0100

    Load data on scroll, when windows resize, to fill the screen...

--- responsiveWebDesign/index.js
+++ responsiveWebDesign/index.js
@@ -1,30 +1,57 @@
-﻿var uri = 'api/realestate';
+﻿var from = 0,
+    to = 10,
+    uri = 'api/realestate/?from=' + from + '&to=' + to;
 
 $(document).ready(function () {
     // Send an AJAX request
-    $.getJSON(uri)
-        .done(function (data) {
-            // On success, 'data' contains a list of products.
-            $.each(data, function (key, item) {
+    loadData(0, 10);
 
-                $('#realEstatesTable > tbody:last').append(
-                    '<tr>' +
-                        '<td>' + item.Id + '</td>' +
-                        '<td>' + item.Company + '</td>' + 
-                        '<td>' + item.City + '</td>' +
-                        '<td>' + item.Location + '</td>' +
-                        '<td>' + item.Type + '</td>' +
-                        '<td>' + item.SquareMeters + '</td>' +
-                        '<td>' + item.Price + '</td>' +
-                        '<td> <a href="' + item.Link + '" target=_blank>' + item.Link + '</td>' +
-                        '<td>' + item.Page + '</td>' +
-                        '<td>' + item.Active + '</td>' +
-                        '<td>' + item.UpdateTime + '</td>' +
-                        '<td>' + item.UpdateDate + '</td>' +
-                        '<td>' + item.InsertTime + '</td>' +
-                        '<td>' + item.InsertDate + '</td>' +
-                    '</tr>'
-                );
-            });
+    $(window).resize(function() {
+        if ($("#realEstatesTable").height() < $(window).height()) {
+            loadData(from, to);
+        }
+    });
+
+    $(window).scroll(function() { //detect page scroll
+        if ($(window).scrollTop() + $(window).height() == $(document).height()) //user scrolled to bottom of the page?
+        {
+            loadData(from, to);
+        }
+    });
+
+
+        function loadData(from, to) {
+        uri = 'api/realestate/?from=' + from + '&to=' + to;
+        $.getJSON(uri)
+            .done(function (data) {
+                from = to;
+                to = to + 10;
+                // On success, 'data' contains a list of products.
+                $.each(data, function (key, item) {
+
+                    $('#realEstatesTable > tbody:last').append(
+                        '<tr>' +
+                            '<td>' + item.Id + '</td>' +
+                            '<td>' + item.Company + '</td>' +
+                            '<td>' + item.City + '</td>' +
+                            '<td>' + item.Location + '</td>' +
+                            '<td>' + item.Type + '</td>' +
+                            '<td>' + item.SquareMeters + '</td>' +
+                            '<td>' + item.Price + '</td>' +
+                            '<td> <a href="' + item.Link + '" target=_blank>' + item.Link + '</td>' +
+                            '<td>' + item.Page + '</td>' +
+                            '<td>' + item.Active + '</td>' +
+                            '<td>' + item.UpdateTime + '</td>' +
+                            '<td>' + item.UpdateDate + '</td>' +
+                            '<td>' + item.InsertTime + '</td>' +
+                            '<td>' + item.InsertDate + '</td>' +
+                        '</tr>'
+                    );
+                });
+                if ($("#realEstatesTable").height() < $(window).height()) {
+                    loadData(from, to);
+                }
         });
+
+    };
 });
